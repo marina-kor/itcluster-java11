097e5479bfa5401427b47290828b35a999b487ec
marina <marina.korotenko@gmail.com>
2021-04-18 21:24:39 +0300

    added models

---
+++ src/main/java/com/mailie/model/PointCategory.java
@@ -0,0 +1,13 @@
+package com.mailie.model;
+
+import lombok.Data;
+import lombok.Setter;
+
+@Data
+@Setter
+public class PointCategory {
+    private String name;
+    private String description;
+
+
+}
