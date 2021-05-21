21967eb2eeace58a69c3215bc8d2097d4c1fd92f
Ivan Kyrylov <kivanval@gmail.com>
2021-05-21 18:40:23 +0300

    Changed to design of the start panel

--- src/main/java/org/itcluster11/commands/StartCommand.java
+++ src/main/java/org/itcluster11/commands/StartCommand.java
@@ -3,7 +3,6 @@ package org.itcluster11.commands;
 import org.itcluster11.services.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.telegram.telegrambots.extensions.bots.commandbot.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.User;
@@ -15,7 +14,7 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class StartCommand extends BotCommand {
+public class StartCommand extends ServiceCommand {
     private final Logger logger = LoggerFactory.getLogger(StartCommand.class.getSimpleName());
     private final ReplyKeyboardMarkup replyKeyboardMarkup;
 
@@ -23,6 +22,8 @@ public class StartCommand extends BotCommand {
         super(identifier, description);
         this.replyKeyboardMarkup = replyKeyboardMarkup;
     }
+
+    @Override
     void sendAnswer(AbsSender absSender, Long chatId, String commandName, String userName, String text) {
         SendMessage message = new SendMessage();
         message.enableMarkdown(true);
@@ -51,11 +52,13 @@ public class StartCommand extends BotCommand {
 
     private void keyBoardBuild(ReplyKeyboardMarkup replyKeyboardMarkup) {
         List<KeyboardRow> keyboard = new ArrayList<>();
-        KeyboardRow keyboardRow = new KeyboardRow();
-        keyboardRow.add("\uD83D\uDE01 Наші пропозиції");
-        keyboardRow.add("\uD83D\uDE43 Створити власний тур");
-        keyboardRow.add("\uD83D\uDE28 Допомога");
-        keyboard.add(keyboardRow);
+        KeyboardRow firstKeyboardRow = new KeyboardRow();
+        firstKeyboardRow.add("\uD83C\uDFE1 Наші пропозиції");
+        firstKeyboardRow.add("\uD83D\uDCDA Допомога");
+        KeyboardRow secondKeyboardRow = new KeyboardRow();
+        secondKeyboardRow.add("\uD83C\uDFD5 Створити власний тур");
+        keyboard.add(firstKeyboardRow);
+        keyboard.add(secondKeyboardRow);
         replyKeyboardMarkup.setKeyboard(keyboard);
     }
 }
